84bacac04dec071e9bae0bb22fb42d796d431351
naginenihari <naginenihari@gmail.com>
2025-11-08 16:09:53 +0530

    Sg

--- locals.tf
+++ locals.tf
@@ -1,9 +1,10 @@
 locals {
+    vpc_id=data.aws_ssm_parameter.vpc_id.value
   common_tags={
     project_nameroject_name=var.project_name
     Environment=var.environment
     Terraform=true
   }
     common_name_suffix="${var.project_name}-${var.environment}"
-    
+
   }
